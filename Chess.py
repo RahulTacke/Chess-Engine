a353575c858328572ffd829bc81e803dc5c5ce73
RahulTacke <104727718+RahulTacke@users.noreply.github.com>
2026-05-26 21:03:16 -0400

    Adding the basic chess library
    Need to continue adding functions

---
+++ Chess.py
@@ -0,0 +1,179 @@
+import numpy as np
+
+class Chess:
+    def __init__(self, test_setup=None):
+        self.board = np.full((8, 8), None, dtype=object) # 8 x 8 board, None for empty squares, (piece, color) tuple for filles squares where White = True, Black = False, and indexed by file-rank (C7 => [2, 6])
+        self.white_move = True # Move tracker
+        self.check = False # Is the current player's king in check?
+        self.castle_rights = ((True, True), (True, True)) # Can players castle? ((White can castle short, White can castle long), (Black can castle short, Black can castle long))
+        self.en_passant = False # If the other player just (on the last move) pushed a pawn two squares, this is equal to the file of that move, otherwise it is False
+        self.checkmate = False # Is the current player's king in checkmate?
+        if test_setup:
+            self.board = test_setup[0]
+            self.white_move = test_setup[1]
+            self.check = test_setup[2]
+            self.castle_rights = test_setup[3]
+            self.en_passant = test_setup[4]
+            self.checkmate = test_setup[5]
+        else:
+            self.board[0, 0] = self.board[7, 0] = ("R", True)
+            self.board[1, 0] = self.board[6, 0] = ("N", True)
+            self.board[2, 0] = self.board[5, 0] = ("B", True)
+            self.board[3, 0] = ("Q", True)
+            self.board[4, 0] = ("K", True)
+            self.board[:, 1] = [("P", True)] * 8
+            self.board[0, 7] = self.board[7, 7] = ("R", False)
+            self.board[1, 7] = self.board[6, 7] = ("N", False)
+            self.board[2, 7] = self.board[5, 7] = ("B", False)
+            self.board[3, 7] = ("Q", False)
+            self.board[4, 7] = ("K", False)
+            self.board[:, 6] = [("P", False)] * 8
+    
+    def __str__(self):
+        out = "\x1b[31m"
+        out += ("0-1" if self.white_move else "1-0") if self.checkmate else "White To Move" if self.white_move else "Black to Move"
+        out += "\x1b[0m\n"
+        for i in range(7, -1, -1):
+            out += "\x1b[31m" + str(i + 1) + "\x1b[0m"
+            for j in range(8):
+                if not self.board[j, i]:
+                    out += "  "
+                elif self.board[j, i][1]:
+                    out += f" \x1b[37m{self.board[j, i][0]}\x1b[0m"
+                else:
+                    out += f" \x1b[30m{self.board[j, i][0]}\x1b[0m"
+            out += "\n"
+        out += " "
+        for i in range(8):
+            out += " \x1b[31m" + "ABCDEFGH"[i] + "\x1b[0m"
+        return out
+
+    # Determines if a proposed move is possible on an ideal board (no pieces in the way, an opponent piece on the appropriate square if a pawn is taking diagonally, a rook if castling)
+    # Assumes start and destination are valid board sqaures
+    @staticmethod
+    def ideally_reachable(piece, start, distX, distY, white):
+        if distX == 0 and distY == 0:
+            return False
+        match piece:
+            case "K":
+                return (start == (4, 0 if white else 7) and abs(distX) == 2 and distY == 0) or (abs(distX) <= 1 and abs(distY) <= 1)
+            case "Q":
+                return abs(distX) - abs(distY) == 0 or distX == 0 or distY == 0
+            case "R":
+                return distX == 0 or distY == 0
+            case "B":
+                return abs(distX) - abs(distY) == 0
+            case "N":
+                return (abs(distX) == 2 and abs(distY) == 1) or (abs(distX) == 1 and abs(distY) == 2)
+            case "P":
+                if distX == 0:
+                    if white:
+                        return distY == 1 or (start[1] == 1 and distY == 2)
+                    return distY == -1 or (start[1] == 6 and distY == -2)
+                return abs(distX) == 1 and (distY == 1 if white else distY == -1)
+
+    # Determines if the specified color is in check on a given board
+    @staticmethod
+    def in_check(board, color):
+        for (file, rank), square in np.ndenumerate(board):
+            if square and square[1] != color:
+                match square[0]:
+                    case "Q" | "R" | "B":
+                        directions = []
+                        match square[0]:
+                            case "Q":
+                                directions += [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (1, 0), (0, -1), (0, 1)]
+                            case "R":
+                                directions += [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                            case "B":
+                                directions += [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+                        for direction in directions:
+                            for i in range(1, 8):
+                                x = file + direction[0] * i
+                                y = rank + direction[1] * i
+                                if x in [-1, 8] or y in [-1, 8]:
+                                    break
+                                if board[x, y]:
+                                    if board[x, y] == ("K", color):
+                                        return True
+                                    break
+                    case "K" | "N" | "P":
+                        targets = []
+                        match square[0]:
+                            case "K":
+                                targets = [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (1, 0), (0, -1), (0, 1)]
+                            case "N":
+                                targets = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
+                            case "P":
+                                targets = [(-1, -1), (1, -1)] if color else [(-1, 1), (1, 1)]
+                        for target in targets:
+                            x = file + target[0]
+                            y = rank + target[1]
+                            if x >= 0 and y >= 0 and x < 8 and y < 8 and board[x, y] and board[x, y] == ("K", color):
+                                return True
+        return False                    
+
+    # Determines if a proposed move is legal
+    # Assumes start and destination are valid board sqaures
+    def legal_move(self, start, dest):
+        piece = self.board[start][0]
+        distX = dest[0] - start[0]
+        distY = dest[1] - start[1]
+        if self.white_move != self.board[start][1] or not Chess.ideally_reachable(piece, start, distX, distY, self.white_move):
+            return False
+        if piece == "P" and (distX == 0) == (bool(self.board[dest]) or (self.en_passant == dest[0] and (start[1] == 4 if self.white_move else start[1] == 3))):
+            return False
+        if self.board[dest] and self.board[dest][1] == self.white_move:
+            return False
+        if piece != "K" or abs(distX) != 2:
+            if abs(distX) == abs(distY):
+                multX = int(distX / abs(distX))
+                multY = int(distY / abs(distY))
+                for i in range(1, abs(distX)):
+                    if self.board[start[0] + multX * i, start[1] + multY * i]:
+                        return False
+            if distX == 0:
+                multY = int(distY / abs(distY))
+                for i in range(1, abs(distY)):
+                    if self.board[start[0], start[1] + multY * i]:
+                        return False
+            if distY == 0:
+                multX = int(distX / abs(distX))
+                for i in range(1, abs(distX)):
+                    if self.board[start[0] + multX * i, start[1]]:
+                        return False
+            checkBoard = self.board.copy()
+            if piece == "P" and distX != 0 and checkBoard[dest] == None:
+                checkBoard[dest[0], start[1]] = None
+            checkBoard[dest] = checkBoard[start]
+            checkBoard[start] = None
+            return not Chess.in_check(checkBoard, self.white_move)
+        if not self.castle_rights[0 if self.white_move else 1][0 if distX == 2 else 1]:
+            return False
+        if distX == 2:
+            for i in range(5, 7):
+                if self.board[i, start[1]]:
+                    return False
+        else:
+            for i in range(1, 4):
+                if self.board[i, start[1]]:
+                    return False
+        if Chess.in_check(self.board, self.white_move):
+            return False
+        checkBoard = self.board.copy()
+        if distX == 2:
+            checkBoard[5, start[1]] = checkBoard[start]
+        else:
+            checkBoard[3, start[1]] = checkBoard[start]
+        checkBoard[start] = None
+        if Chess.in_check(checkBoard, self.white_move):
+            return False
+        if distX == 2:
+            checkBoard[6, start[1]] = self.board[start]
+            checkBoard[5, start[1]] = checkBoard[7, start[1]]
+            checkBoard[7, start[1]] = None
+        else:
+            checkBoard[2, start[1]] = self.board[start]
+            checkBoard[3, start[1]] = checkBoard[0, start[1]]
+            checkBoard[0, start[1]] = None
+        return not Chess.in_check(checkBoard, self.white_move)
